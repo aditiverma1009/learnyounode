15c115a0fc60081f6a069c401feb38c3e1743589
aditiverma1009 <aditiverma1009@gmail.com>
2018-02-05 00:22:36 +0530

    httpclient (ex 7) : check valid url

--- httpclient.js
+++ httpclient.js
@@ -1,38 +1,17 @@
 const http = require('http');
 
-const validateIP = (validatearg) => {
-  // console.log(validatearg, '****');
-  if (validatearg === null || validatearg === undefined) {
-    return true;
-  }
-  return false;
-};
-
 function httpclient(arg1, callback) {
-  let rawData = '';
-  if (validateIP(arg1) === true) {
-    callback('error');
-  }
+  // const rawData = '';
+  // if (arg1 === undefined || arg1 === null) {
+  //   return callback('err');
+  // }
   // const arg1 = process.argv[2];
   http.get(arg1, (response) => {
-    response.setEncoding('utf8');
+    response.setEncoding('utf-8');
     // console.log(dataUTF);
-    response.on('err', (error) => {
-      console.log(error);
-      callback('error');
-    });
 
-    response.on('data', (chunk) => {
-      rawData = `${chunk}`;
-      console.log(rawData);
-      callback('data');
-    });
-
-    response.on('end', (end) => {
-      callback('end');
-      console.log(end);
-    });
-  });
+    return callback(response.statusCode);
+  }).on('error', e => callback(e.code));
 }
 
 // httpclient();
--- httpclient_test.js
+++ httpclient_test.js
@@ -1,34 +1,12 @@
 // testing for sync functions
 const tester = require('./httpclient');
 
-describe('To check all invalid cases', () => {
-  test('null', (done) => {
-    function callback(data) {
-      expect(data).toBe('error');
+describe('To check all valid cases', () => {
+  test('with a valid url', (done) => {
+    const callback = (data) => {
+      expect(data).toBe(200);
       done();
-    }
-    tester(null, callback);
-    // global.console.log = jest.fn();
-  });
-  test('undefined', (done) => {
-    function callback(data) {
-      expect(data).toBe('error');
-      done();
-    }
-    tester(undefined, callback);
-    // global.console.log = jest.fn();
-  });
-});
-// not working with url - but printing website response
-describe('To check all possible cases', () => {
-  test('testing url', (done) => {
-    function callback(data) {
-      expect(data).toMatch('data');
-      done();
-    }
+    };
     tester('http://www.oplador.com', callback);
-    // global.console.log = jest.fn();
   });
 });
-// error test case
-// pass http://www.oplador.com/404
