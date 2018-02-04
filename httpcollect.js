f8c379b76b6486c43a6ac287afe82e9c83c88bd5
aditiverma1009 <aditiverma1009@gmail.com>
2018-02-05 01:04:37 +0530

    httpcollect & httpcollect.test.js (ex 8) : tested for valid url

--- httpcollect.js
+++ httpcollect.js
@@ -1,30 +1,17 @@
 const bl = require('bl');
 const http = require('http');
 // const fs = require('fs');
-
-
-const validateIP = (validatearg) => {
-  // console.log(validatearg, '****');
-  if (validatearg === null || validatearg === undefined) {
-    return true;
-  }
-  return false;
-};
-
+// bl (Buffer List)
+// response.pipe(bl(function (err, data) { /* ... */ }))
 function httpcollect(arg1, callback) {
-  if (validateIP(arg1) === true) {
-    callback(false);
-    // console.log('error');
-  }
   http.get(arg1, (response) => {
     response.pipe(bl((err, data) => {
-      if (err) { return console.error(err); }
+      if (err) { return callback(err); }
       const dataString = data.toString();
       console.log(dataString.length);
-      console.log(dataString);
-      return true;
+      // console.log(dataString);
+      return callback(response.statusCode);
     }));
-    callback(true);
   });
 }
 // httpcollect();
